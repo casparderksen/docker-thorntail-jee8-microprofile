499ea2afbcc9584d04505856bfd1f2269eac5a2b
Caspar Derksen <caspard@me.com>
2018-12-19 11:34:50 +0100

    added logging and javadoc

--- src/main/java/nl/casparderksen/rest/RuntimeExceptionMapper.java
+++ src/main/java/nl/casparderksen/rest/RuntimeExceptionMapper.java
@@ -1,18 +1,29 @@
 package nl.casparderksen.rest;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Provider
+@Slf4j
 public class RuntimeExceptionMapper implements ExceptionMapper<RuntimeException> {
 
+    /**
+     * Maps uncaught runtime exceptions to internal server errors, in order to avoid returning stack traces.
+     * If the exception is a {@link WebApplicationException}, its response is returned.
+     * Otherwise, an internal server error response is returned and the exception is logged.
+     * @param exception the exception
+     * @return the response
+     */
     @Override
     public Response toResponse(RuntimeException exception) {
         if(exception instanceof WebApplicationException) {
             return ((WebApplicationException) exception).getResponse();
         }
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        log.error("internal server error", exception);
+        return Response.serverError().build();
     }
 }
