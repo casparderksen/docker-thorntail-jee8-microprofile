6a9629fbc899b7f9f779893c8e13809e7dd1b9f2
Caspar Derksen <caspar.derksen@tennet.eu>
2019-08-30 18:09:33 +0200

    fix for running in Docker

--- myapp/src/main/docker/myapp/entrypoint.sh
+++ myapp/src/main/docker/myapp/entrypoint.sh
@@ -4,7 +4,7 @@
 : ${JAVA_OPTS:="-XX:MaxRAMFraction=1"}
 : ${JAVA_EXTRA_OPTS:="-Djava.net.preferIPv4Stack=true -Djava.security.egd=file:/dev/./urandom"}
 : ${TX_NODE_ID:=$(hostname)}
-export THORNTAIL_JAVA_OPTS="${JAVA_OPTS} ${JAVA_EXTRA_OPTS} -Djboss.tx.node.id=${TX_NODE_ID}"
 
 # Run application
-exec /usr/bin/java ${THORNTAIL_JAVA_OPTS} -jar myapp.jar $*
+jar_file=$(ls *.jar)
+exec /usr/bin/java ${JAVA_OPTS} ${JAVA_EXTRA_OPTS} -Djboss.tx.node.id=${TX_NODE_ID} -jar ${jar_file} $*
