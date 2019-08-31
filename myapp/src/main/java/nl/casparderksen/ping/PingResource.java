475aabb4310fe47b53da03de2c3b9fc03c5c97af
Caspar Derksen <caspard@me.com>
2019-08-31 13:08:51 +0200

    refactoring

--- myapp/src/main/java/nl/casparderksen/ping/PingResource.java
+++ myapp/src/main/java/nl/casparderksen/ping/PingResource.java
@@ -3,7 +3,7 @@ package nl.casparderksen.ping;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
@@ -24,44 +24,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class PingResource {
 
     @Inject
-    @ConfigProperty(name = "project.groupId")
-    private String projectGroupId;
-
-    @Inject
-    @ConfigProperty(name = "project.artifactId")
-    private String projectArtifactId;
-
-    @Inject
-    @ConfigProperty(name = "project.version")
-    private String projectVersion;
-
-    @Inject
-    @ConfigProperty(name = "git.remote.origin.url")
-    private String gitRemoteOriginURL;
-
-    @Inject
-    @ConfigProperty(name = "git.build.time")
-    private String gitBuildTime;
-
-    @Inject
-    @ConfigProperty(name = "git.branch")
-    private String gitBranch;
-
-    @Inject
-    @ConfigProperty(name = "git.commit.id")
-    private String gitCommitId;
-
-    @Inject
-    @ConfigProperty(name = "git.commit.id.abbrev")
-    private String gitCommitIdAbbrev;
-
-    @Inject
-    @ConfigProperty(name = "git.dirty")
-    private boolean gitDirty;
-
-    @Inject
-    @ConfigProperty(name = "git.tags", defaultValue = "")
-    private String gitTags;
+    private Config config;
 
     @GET
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
@@ -75,33 +38,33 @@ public class PingResource {
             monotonic = true,
             displayName = "Number of pings",
             description = "Metric to show how many times the ping endpoint was called")
-    public PingResponse ping() {
-        return new PingResponse(
-                projectGroupId,
-                projectArtifactId,
-                projectVersion,
-                gitRemoteOriginURL,
-                gitBuildTime,
-                gitBranch,
-                gitCommitId,
-                gitCommitIdAbbrev,
-                gitDirty,
-                gitTags);
+    public ApplicationInfo ping() {
+        return new ApplicationInfo(
+                config.getValue("project.groupId", String.class),
+                config.getValue("project.artifactId", String.class),
+                config.getValue("project.version", String.class),
+                config.getValue("git.remote.origin.url", String.class),
+                config.getValue("git.build.time", String.class),
+                config.getValue("git.commit.id", String.class),
+                config.getValue("git.commit.id.abbrev", String.class),
+                config.getValue("git.branch", String.class),
+                config.getValue("git.dirty", Boolean.class),
+                config.getOptionalValue("git.tags", String.class).orElse(""));
     }
 
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
     @XmlRootElement
-    public static class PingResponse {
+    public static class ApplicationInfo {
         private String projectGroupId;
         private String projectArtifactId;
         private String projectVersion;
         private String gitRemoteOriginURL;
         private String gitBuildTime;
-        private String gitBranch;
         private String gitCommitId;
         private String gitCommitIdAbbrev;
+        private String gitBranch;
         private boolean gitDirty;
         private String gitTags;
     }
