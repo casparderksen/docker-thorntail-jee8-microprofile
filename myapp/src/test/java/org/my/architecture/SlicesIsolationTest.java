53771461d6b3f77297827193ef6b55b2d0d6f785
Caspar Derksen <caspard@me.com>
2019-09-03 16:26:41 +0200

    removed cycles test (require independent slices)

--- myapp/src/test/java/org/my/architecture/SlicesIsolationTest.java
+++ myapp/src/test/java/org/my/architecture/SlicesIsolationTest.java
@@ -7,20 +7,17 @@ import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
 
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
-@AnalyzeClasses(packages = "org.myapp")
+@AnalyzeClasses(packages = "org.my.app")
 class SlicesIsolationTest {
 
     @ArchTest
-    static final ArchRule SLICES_SHOULD_BE_FREE_OF_CYCLES =
-            SlicesRuleDefinition.slices().matching("org.my.app.(*)..").should().beFreeOfCycles();
-
-    @ArchTest
     static final ArchRule SLICES_SHOULD_NOT_DEPEND_ON_EACH_OTHER =
-            SlicesRuleDefinition.slices().matching("org.my.app.(**)").should().notDependOnEachOther();
+            SlicesRuleDefinition.slices().matching("org.my.app.(*)..").should().notDependOnEachOther();
 
+    // TODO test not working
     @ArchTest
     static final ArchRule UTILS_SHOULD_NOT_DEPEND_ON_APP = layeredArchitecture()
             .layer("Utilities").definedBy("org.my.util..")
             .layer("Application").definedBy("org.my.app..")
-            .whereLayer("Utilities").mayNotBeAccessedByAnyLayer();
+            .whereLayer("Application").mayNotBeAccessedByAnyLayer();
 }
